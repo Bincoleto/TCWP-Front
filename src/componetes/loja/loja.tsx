2117c1d44e8f4e10730d28937aff2776e121c76c
Marcio Bincoleto <marciobincoletojunior@gmail.com>
2025-09-20 01:03:44 -0300

    Criado componente Loja e adicionado no compornete home

---
+++ src/componetes/loja/loja.tsx
@@ -0,0 +1,19 @@
+function Loja(){
+    return(
+        <>
+            <div className="loja">
+                <div className="loja-div1">
+                    <div className="loja-titulo">
+                        <img src="public/icones/icone-img-loja.png" alt="icone-twcp"/>
+                        <h2>Pedais em Destaque</h2>
+                    </div>
+                    <p>Mais populares e bem avaliados</p>
+                </div>
+                
+            </div>
+
+        </>
+    )
+}
+
+export default Loja;
--- src/paginas/home/home.tsx
+++ src/paginas/home/home.tsx
@@ -1,11 +1,13 @@
 import Banner from "../../componetes/banner/banner"
 import Cabecalho from "../../componetes/cabecalho/cabecalho"
+import Loja from "../../componetes/loja/loja"
 
 function Home(){
     return(
         <div>
             <Cabecalho/>
             <Banner/>
+            <Loja/>
         </div>
     )
 }
