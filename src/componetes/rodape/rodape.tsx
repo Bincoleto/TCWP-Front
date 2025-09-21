34a032af1f001755131fc0149ae5a508ad41b01d
Marcio Bincoleto <marciobincoletojunior@gmail.com>
2025-09-21 02:38:35 -0300

    Finalizado layout rodapé

---
+++ src/componetes/rodape/rodape.tsx
@@ -0,0 +1,20 @@
+function Rodape(){
+    return(
+        <div className="rodape-principal">
+
+            <div className="rodape-div1">
+                <div className="rodape-div1-item">
+                    <div>
+                        <img src="public/icone-tcwp.svg" alt="Imagem TCWP" />
+                        <h5>The Crazy World Pedals</h5>
+                    </div>
+                    <p>Sua loja de pedais novos e usados</p>
+                </div>
+            </div>
+
+        <p>2025 The Crazy World Pedals. Todos os direitos reservados</p>
+        </div>
+    )
+}
+
+export default Rodape;
--- src/paginas/home/home.tsx
+++ src/paginas/home/home.tsx
@@ -2,6 +2,7 @@ import Banner from "../../componetes/banner/banner"
 import Cabecalho from "../../componetes/cabecalho/cabecalho"
 import Loja from "../../componetes/loja/loja"
 import RecebaNovidades from "../../componetes/novidades/novidades"
+import Rodape from "../../componetes/rodape/rodape"
 
 function Home(){
     return(
@@ -10,6 +11,7 @@ function Home(){
             <Banner/>
             <Loja/>
             <RecebaNovidades/>
+            <Rodape/>
         </div>
     )
 }
